3c07efd215e91ff361ac4438686d7874152f036a
Xzib <syedzohaibali94@gmail.com>
2019-07-26 16:54:45 +0500

    Dashboard started

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/DashboardLayout")
+@app.route("/dashboard")
 def dashboard():
-    return render_template("DashboardLayout.html")
+    return render_template("dashboard.html")
